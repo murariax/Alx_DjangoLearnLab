d490e5abeabfd8792a958a3684976e92acabacf6
Max <murariax@gmail.com>
2025-07-10 14:35:43 +0300

    added admin panel

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns shown in list
+    list_filter = ('publication_year', 'author')            # Filters on the side
+    search_fields = ('title', 'author')                     # Search box fields
