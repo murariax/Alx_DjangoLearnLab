41637c68cd77ec47a6b587f7d37f39f8d81b30de
Max <murariax@gmail.com>
2025-07-18 15:30:34 +0300

    used class approach to implement relationship/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-# relationship_app/views.py
 
 # relationship_app/views.py
 
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Library
 
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
