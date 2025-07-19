a9ab2cf8e1534e2ad1a44c3c4fa90a026bd2cc0c
Max <murariax@gmail.com>
2025-07-19 09:21:18 +0300

    implemented admin panel

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
-]
+
 
 urlpatterns = [
     path('books/', list_books, name='book-list'),  # Function-based view
@@ -13,5 +13,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),  # ✅ function-based for register
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # ✅ class-based
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('admin-dashboard/', views.admin_view, name='admin-view'),
+    path('librarian-dashboard/', views.librarian_view, name='librarian-view'),
+    path('member-dashboard/', views.member_view, name='member-view')
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,15 +4,38 @@ from .models import Library
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views.generic.detail import DetailView
+from .models import Book, Library
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
 # Create your views here.
 
 # relationship_app/views.py
 
-# relationship_app/views.py
 
-from django.views.generic.detail import DetailView
-from .models import Book, Library
+# Role-check functions
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 
 # ✅ Function-based view: List all books
 def list_books(request):
