8a251504c515767f7d79dfc08fb1d403a7d891a8
Max <murariax@gmail.com>
2025-07-19 08:40:25 +0300

    implemented authentication views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,9 +2,16 @@
 
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from .views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('books/', list_books, name='book-list'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),  # Class-based view
 ]
 
+
+urlpatterns += [
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+]
