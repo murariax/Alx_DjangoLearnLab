d701439f38fb319978bc10710881fb5e6a7912f7
Max <murariax@gmail.com>
2025-08-06 08:36:03 +0300

    Please enter the commit message for your changes. Lines starting ear with '#' will be ignored, and an empty message aborts the commit.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from .views import BookList
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('books/', BookList.as_view(), name='book-list'),
 ]
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
