86ad81932a6a76ffbfe547a93f2f904a6db62ba2
Max <murariax@gmail.com>
2025-08-06 08:32:00 +0300

    corrected prject structure

---
+++ advanced_api_project/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Author, Book
+from datetime import datetime
+
+class BookSerializer(serializers.ModelSerializer):
+    """Serializes Book data with custom validation"""
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def validate_publication_year(self, value):
+        if value > datetime.now().year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    """Serializes Author data with nested books"""
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'books']
---
+++ advanced_api_project/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # We'll add views here later
+]
